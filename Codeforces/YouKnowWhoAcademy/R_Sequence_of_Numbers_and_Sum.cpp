73d3ba11c2f8f241f8e38dc0f28f763007ad3557
Md. Abdullah Al Mamun <mamuntherealone@gmail.com>
2025-12-17 21:31:44 +0600

    Add files via upload

---
+++ Codeforces/YouKnowWhoAcademy/R_Sequence_of_Numbers_and_Sum.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m;
+    while(cin >> n >> m)
+    {
+        if(n <= 0 || m <= 0)
+        {
+            break;
+        }
+
+        int start = min(n, m);
+        int end = max(n,m);
+        int sum = 0;
+
+        for(int i = start; i <= end; i++)
+        {
+            cout << i << " ";
+            sum += i;
+        }
+
+        cout << "sum =" << sum << endl;
+    }
+    return 0;
+}
